c6917a2f439b99078e67310230f1d0cfa0de8a7b
Joe Cross <joe.mcross@gmail.com>
2013-08-07 01:03:56 -0700

    Add test helper for creating users

--- tests/builder_tests.py
+++ tests/builder_tests.py
@@ -3,7 +3,6 @@ import unittest
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.pool import NullPool
 from interrogate import Builder
 
 
@@ -11,33 +10,18 @@ class InterrogateTestCase(unittest.TestCase):
 
     def valid_builder_args(self):
         model = self.model
-        type_constraints = {
-            'string': [
-                'name',
-                'email'
-            ],
-            'numeric': [
-                'age',
-                'height'
-            ],
-            'nullable': [
-                'email',
-                'height'
-            ]
-        }
         query_constraints = {
             'breadth': None,
             'depth': 32,
             'elements': 64
         }
-        return [model, type_constraints, query_constraints]
+        return [model, query_constraints]
 
-    def make_builder(self, model=None, type_constraints=None, query_constraints=None):
+    def make_builder(self, model=None, query_constraints=None):
         dm, dt, dq = self.valid_builder_args()
 
         return Builder(
             model or dm,
-            type_constraints or dt,
             query_constraints or dq
         )
 
@@ -51,10 +35,15 @@ class InterrogateTestCase(unittest.TestCase):
             email = Column(String)
             age = Column(Integer)
             height = Column(Integer)
-        engine = create_engine("sqlite://", poolclass=NullPool)
+        engine = create_engine("sqlite://", echo=True)
         Base.metadata.create_all(engine)
         self.model = User
         self.session = sessionmaker(bind=engine)()
 
     def tearDown(self):
         self.session.close()
+
+    def add_user(self, **kwargs):
+        user = self.model(**kwargs)
+        self.session.add(user)
+        self.session.commit()
